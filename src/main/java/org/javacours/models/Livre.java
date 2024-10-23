6ac04ea35ddf4e9d4d5747184bccd7c9ce6fdcfe
rozennrd <r_renaud@gmx.fr>
2024-10-23 08:41:59 +0200

    init: first commit

---
+++ src/main/java/org/javacours/models/Livre.java
@@ -0,0 +1,9 @@
+package org.javacours.models;
+
+public class Livre {
+    private String isbn;
+    private String title;
+    private String description;
+    private String authorName;
+    private double price;
+}
---
+++ src/main/java/org/javacours/models/User.java
@@ -0,0 +1,8 @@
+package org.javacours.models;
+
+public class User {
+    private String id;
+    private String login;
+    private String password;
+}
+    
